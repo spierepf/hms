9a7324196e6ae609a6a02a72a5628d74d6ee9e51
Peter-Frank Spierenburg <spierepf@hotmail.com>
2018-02-21 21:53:29 -0400

    User is a Person

--- grails-app/domain/hms/User.groovy
+++ grails-app/domain/hms/User.groovy
@@ -7,7 +7,7 @@ import grails.compiler.GrailsCompileStatic
 @GrailsCompileStatic
 @EqualsAndHashCode(includes='username')
 @ToString(includes='username', includeNames=true, includePackage=false)
-class User implements Serializable {
+class User extends Person implements Serializable {
 
     private static final long serialVersionUID = 1
 
